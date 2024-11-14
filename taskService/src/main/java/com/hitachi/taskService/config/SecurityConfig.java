546d45fdc64dc67dbf528c53a4cfa776a5648f17
omar2432 <omar.ahmed2432@gmail.com>
2024-11-14 17:52:23 +0200

    -added KeycloakRoleConverter -in pom.xml added spring-boot-starter-oauth2-resource-server dependency -in application.properties added spring.security.oauth2.resourceserver.jwt.jwk-set-uri -in docker-compose.yml added the environment variable spring_security_oauth2_resourceserver_jwt_jwk-set-uri -changed the SecurityConfig to secure the service as a resource server

--- taskService/src/main/java/com/hitachi/taskService/config/SecurityConfig.java
+++ taskService/src/main/java/com/hitachi/taskService/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.hitachi.taskService.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http    .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/tasks/**").permitAll() // Allow public access to your endpoint
-                        .anyRequest().authenticated() // Protect other endpoints
-                );
-//                .oauth2Login().disable(); // Disable the default OAuth2 login flow
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        JwtAuthenticationConverter jwtAuthenticationConverter=new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/tasks/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter)
+                ));
         return http.build();
     }
+
 }
